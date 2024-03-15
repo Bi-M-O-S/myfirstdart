380fa16bf163e9c8572fba9fb45bf3de7e324de4
Bi-M-O-S <160823740+Bi-M-O-S@users.noreply.github.com>
2024-03-15 05:00:11 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  //declaring variables
+  String name = 'Bilal';
+  String address = 'Uganda';
+  int age = 25;
+  double height = 1.73;
+  bool isMarried = false;
+  Map <String, int> perentsAge = {
+    'mary' : 40,
+    'John' : 60
+  };
+  //printing variables
+  print('My name is $name. I live in $address. My age is $age. Married status is $isMarried. I am $height tall. My parents are $perentsAge.');
+}
